b6d2f27fc68f49999464f6a20ec14fa3a52b67b1
EgydGIT <egyd.eszter@gmail.com>
2022-03-23 13:31:40 +0100

    sudop #p.4: TimeSheet

---
+++ src/main/java/timesheet/TimeSheetItem.java
@@ -0,0 +1,55 @@
+package timesheet;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TimeSheetItem {
+
+    private Employee employee;
+    private Project project;
+    private LocalDateTime beginDate;
+    private LocalDateTime endDate;
+
+    public TimeSheetItem(Employee employee, Project project, LocalDateTime beginDate, LocalDateTime endDate) {
+        if (endDate.isBefore(beginDate)) {
+            throw new IllegalArgumentException("End date (" + endDate + ") can not be before begin date (" + beginDate + ")! ");
+        }
+        if (!beginDate.toLocalDate().equals(endDate.toLocalDate())) {
+            throw new IllegalArgumentException("Begin date (" + beginDate + ") must be in the same day, as end date (" + endDate + ")! ");
+        }
+        this.employee = employee;
+        this.project = project;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+    }
+
+    public long hoursBetweenDates() {
+        return Duration.between(beginDate, endDate).toHours();
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public LocalDateTime getBeginDate() {
+        return beginDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSheetItem{" +
+                "employee=" + employee +
+                ", project=" + project +
+                ", beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
